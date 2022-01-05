6322923f88535cfa388f7e14bdd9e060e563f553
Sébastien MB <seb@essembeh.org>
2022-01-05 21:12:18 +0100

    ✨ Add modes
    - remove --group
    - add diff/wdiff/simple modes
    - update documentation

--- README.md
+++ README.md
@@ -7,7 +7,39 @@
 
 Command line tool to compare *properties* files and print differences with colors as if you were using `diff` or `colordiff` tools.
 
-![Example](capture.png)
+Even if *properties* files are text files, using directly `diff` is not that efficient because of key/value pairs order or format (for example using `=` or `[space]=[space]` as separator, double quoting values...). `properties-diff` compare key/value pairs but not the order nor the format.
+
+# Usage
+
+```sh
+$ properties-diff --help
+usage: properties-diff [-h] [-q] [--quote] [--sep SEP] [-m {simple,diff,wdiff}] left.properties right.properties
+
+positional arguments:
+  left.properties       left file to compare
+  right.properties      right file to compare
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -q, --quiet           print less information
+  --quote               use double quotes for values, example: foo="bar"
+  --sep SEP             key/value separator, default is '='
+  -m {simple,diff,wdiff}, --mode {simple,diff,wdiff}
+                        select a format to show differences: using colors only (simple), using diff-like format (diff) or wdiff-like
+                        (wdiff) format. Default is 'diff'
+```
+
+## Modes
+
+
+You can see differences between the properties files using 3 modes using `--mode <MODE>`
+* `simple`, based on colors, *red* for removed lines, *green* for added lines
+* `diff`, prints the changes like `diff` tool would do (this is the default mode)
+* `wdiff`, prints the changes like `wdiff` tool would do
+
+![simple](images/simple.png)
+![diff](images/diff.png)
+![wdiff](images/wdiff.png)
 
 
 # Install
--- properties_diff/cli.py
+++ properties_diff/cli.py
@@ -2,6 +2,7 @@
 
 import sys
 from argparse import ArgumentParser
+from datetime import datetime
 from pathlib import Path
 from typing import Dict
 
@@ -10,9 +11,9 @@ from colorama import Fore
 
 def properties_to_dict(file: Path, separator="=", comment_char="#") -> Dict[str, str]:
     if not file.exists():
-        raise FileExistsError(f"Cannot find file {Fore.YELLOW}{file}{Fore.RESET}")
+        raise FileExistsError(f"Cannot find file {file}")
     out = {}
-    for line in map(str.strip, file.read_text().splitlines()):
+    for lineno, line in enumerate(map(str.strip, file.read_text().splitlines())):
         if len(line) == 0:
             # empty line
             pass
@@ -21,9 +22,13 @@ def properties_to_dict(file: Path, separator="=", comment_char="#") -> Dict[str,
             pass
         elif separator not in line:
             # invalid line
-            raise SyntaxError(
-                f"Invalid line in {Fore.YELLOW}{file}{Fore.RESET}: {Fore.RED}{line}{Fore.RESET}"
+            se = SyntaxError(
+                f"Invalid file, no separator '{separator}' on line {lineno}"
             )
+            se.lineno = lineno
+            se.filename = str(file)
+            se.text = line
+            raise se
         else:
             index = line.index(separator)
             key = line[0:index].strip()
@@ -36,89 +41,137 @@ def properties_to_dict(file: Path, separator="=", comment_char="#") -> Dict[str,
 
 def run(args=None):
     parser = ArgumentParser()
-    parser.add_argument("-q", "--quiet", action="store_true", help="less verbose")
     parser.add_argument(
-        "-p", "--pretty", action="store_true", help='print properties as key = "value"'
-    )
-    parser.add_argument(
-        "-g",
-        "--group-updated",
+        "-q",
+        "--quiet",
         action="store_true",
-        help="group updated properties",
+        help="print less information",
     )
     parser.add_argument(
-        "left", type=Path, metavar="left.properties", help="left file to compare"
+        "--quote",
+        action="store_true",
+        help='use double quotes for values, example: foo="bar"',
     )
     parser.add_argument(
-        "right", type=Path, metavar="right.properties", help="right file to compare"
+        "--sep",
+        default="=",
+        help="key/value separator, default is '='",
+    )
+    parser.add_argument(
+        "-m",
+        "--mode",
+        choices=["simple", "diff", "wdiff"],
+        default="diff",
+        help="select a format to show differences: using colors only (simple), using diff-like format (diff) or wdiff-like (wdiff) format. Default is 'diff'",
+    )
+    parser.add_argument(
+        "left",
+        type=Path,
+        metavar="left.properties",
+        help="left file to compare",
+    )
+    parser.add_argument(
+        "right",
+        type=Path,
+        metavar="right.properties",
+        help="right file to compare",
     )
 
     args = parser.parse_args(args)
 
-    prefix_added, prefix_removed = "+ ", "- "
-    if args.quiet:
-        prefix_added, prefix_removed = "", ""
+    def quote(data: dict, key: str):
+        text = data.get(key, "")
+        return f'"{text}"' if args.quote else text
 
-    def print_kv(data: dict, key: str):
-        if args.pretty:
-            return f'{key} = "{data[key]}"'
-        return f"{key}={data[key]}"
+    def date(file: Path):
+        return datetime.isoformat(
+            datetime.fromtimestamp(file.stat().st_mtime), timespec="seconds", sep=" "
+        )
 
     try:
-        left = properties_to_dict(args.left)
-        assert (
-            len(left) > 0
-        ), f"Cannot find any property in {Fore.YELLOW}{args.left}{Fore.RESET}"
-        right = properties_to_dict(args.right)
-        assert (
-            len(right) > 0
-        ), f"Cannot find any property in {Fore.YELLOW}{args.right}{Fore.RESET}"
+        left = properties_to_dict(args.left, separator=args.sep.strip())
+        assert len(left) > 0, f"Cannot find any property in {args.left}"
+        right = properties_to_dict(args.right, separator=args.sep.strip())
+        assert len(right) > 0, f"Cannot find any property in {args.right}"
 
         added = [key for key in sorted(right) if key not in left]
-        if len(added):
-            print(f"{Fore.BLUE}Added properties in {args.right}")
-            if not args.quiet:
-                print(f"{Fore.YELLOW}--- {args.left}{Fore.RESET}")
-                print(f"{Fore.YELLOW}+++ {args.right}{Fore.RESET}")
-            for key in added:
-                print(f"{Fore.GREEN}{prefix_added}{print_kv(right, key)}{Fore.RESET}")
-
         removed = [key for key in sorted(left) if key not in right]
-        if len(removed):
-            print(f"{Fore.BLUE}Removed properties from {args.left}")
-            if not args.quiet:
-                print(f"{Fore.YELLOW}--- {args.left}{Fore.RESET}")
-                print(f"{Fore.YELLOW}+++ {args.right}{Fore.RESET}")
-            for key in removed:
-                print(f"{Fore.RED}{prefix_removed}{print_kv(left, key)}{Fore.RESET}")
-
         updated = [
             key for key in sorted(left) if key in right and left[key] != right[key]
         ]
-        if len(updated):
-            print(
-                f"{Fore.BLUE}Updated properties from {args.left} to {args.right}{Fore.RESET}"
-            )
+        if len(added) == 0 and len(removed) == 0 and len(updated) == 0:
+            print(f"Files {args.left} and {args.right} are similar")
+        else:
             if not args.quiet:
-                print(f"{Fore.YELLOW}--- {args.left}{Fore.RESET}")
-                print(f"{Fore.YELLOW}+++ {args.right}{Fore.RESET}")
-            if args.group_updated:
-                for key in updated:
-                    print(
-                        f"{Fore.RED}{prefix_removed}{print_kv(left, key)}{Fore.RESET}"
-                    )
-                for key in updated:
-                    print(
-                        f"{Fore.GREEN}{prefix_added}{print_kv(right, key)}{Fore.RESET}"
-                    )
-            else:
-                for key in updated:
-                    print(
-                        f"{Fore.RED}{prefix_removed}{print_kv(left, key)}{Fore.RESET}"
-                    )
-                    print(
-                        f"{Fore.GREEN}{prefix_added}{print_kv(right, key)}{Fore.RESET}"
-                    )
-    except BaseException as e:
-        print(e, file=sys.stderr)
+                print(f"{Fore.YELLOW}--- {args.left}{Fore.RESET}    {date(args.left)}")
+                print(
+                    f"{Fore.YELLOW}+++ {args.right}{Fore.RESET}    {date(args.right)}"
+                )
+
+            if len(removed):
+                print(f"{Fore.BLUE}# Only in {args.left}{Fore.RESET}")
+                if args.mode == "simple":
+                    for key in removed:
+                        print(
+                            f"{Fore.RED}{key}{args.sep}{quote(left, key)}{Fore.RESET}"
+                        )
+                elif args.mode == "diff":
+                    for key in removed:
+                        print(
+                            f"{Fore.RED}- {key}{args.sep}{quote(left, key)}{Fore.RESET}"
+                        )
+                elif args.mode == "wdiff":
+                    for key in removed:
+                        print(
+                            f"{Fore.RED}[-{key}{args.sep}{quote(left, key)}-]{Fore.RESET}"
+                        )
+
+            if len(added):
+                print(f"{Fore.BLUE}# Only in {args.right}{Fore.RESET}")
+                if args.mode == "simple":
+                    for key in added:
+                        print(
+                            f"{Fore.GREEN}{key}{args.sep}{quote(right, key)}{Fore.RESET}"
+                        )
+                elif args.mode == "diff":
+                    for key in added:
+                        print(
+                            f"{Fore.GREEN}+ {key}{args.sep}{quote(right, key)}{Fore.RESET}"
+                        )
+                elif args.mode == "wdiff":
+                    for key in added:
+                        print(
+                            f"{Fore.GREEN}{{+{key}{args.sep}{quote(right, key)}+}}{Fore.RESET}"
+                        )
+
+            if len(updated):
+                print(
+                    f"{Fore.BLUE}# Updated from {args.left} to {args.right}{Fore.RESET}"
+                )
+                if args.mode == "simple":
+                    for key in updated:
+                        print(
+                            f"{Fore.RED}{key}{args.sep}{quote(left, key)}{Fore.RESET}"
+                        )
+                    for key in updated:
+                        print(
+                            f"{Fore.GREEN}{key}{args.sep}{quote(right, key)}{Fore.RESET}"
+                        )
+                elif args.mode == "diff":
+                    for key in updated:
+                        print(
+                            f"{Fore.RED}- {key}{args.sep}{quote(left, key)}{Fore.RESET}"
+                        )
+                        print(
+                            f"{Fore.GREEN}+ {key}{args.sep}{quote(right, key)}{Fore.RESET}"
+                        )
+                elif args.mode == "wdiff":
+                    for key in updated:
+                        print(
+                            f"{key}{args.sep}{Fore.RED}[-{quote(left, key)}-]{Fore.GREEN}{{+{quote(right, key)}+}}{Fore.RESET}"
+                        )
+    except BaseException as exc:
+        print(f"{Fore.RED}ERROR: {exc}{Fore.RESET}", file=sys.stderr)
+        if isinstance(exc, SyntaxError):
+            print(f"{Fore.YELLOW}{exc.filename}:{exc.lineno}{Fore.RESET}  {exc.text}")
         sys.exit(1)
